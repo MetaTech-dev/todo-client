4573a4a8fd5bb3f8688bc2aa8f503ce0f2018d76
Zaq <115123603+zaqreynolds@users.noreply.github.com>
2023-04-26 14:33:05 -0700

    Update src/App.js
    Co-authored-by: MetaMatt <matt@metatech.dev>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { Button } from "@mui/material";
-import CssBaseline from "@mui/material/CssBaseline";
+import { Button, CssBaseline } from "@mui/material";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
