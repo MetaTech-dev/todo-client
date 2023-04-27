e27412893131b5f670a99145f93da306f9acc262
Zaq Reynolds <zaqreynolds@gmail.com>
2023-04-27 15:42:11 -0700

    wraps Outlet in flex Box

--- src/Layout.js
+++ src/Layout.js
@@ -1,7 +1,20 @@
+import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-  return <Outlet />;
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        flexDirection: "column",
+        overflow: "hidden",
+      }}
+    >
+      <Outlet />
+    </Box>
+  );
 };
 
 export default Layout;
