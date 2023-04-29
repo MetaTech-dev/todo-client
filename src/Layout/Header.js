fd3c009c5684d5f711bc29a6171a8df5807199db
MetaMatt <matt@metatech.dev>
2023-04-28 17:49:33 -0700

    feat(Header): add IconButton and title

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,4 +1,4 @@
-import { Toolbar, Box } from "@mui/material";
+import { Toolbar, Box, IconButton, Typography } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import MenuIcon from "@mui/icons-material/Menu";
 import Button from "@mui/material/Button";
@@ -7,7 +7,18 @@ const Header = () => {
   return (
     <AppBar position="static" sx={{ flexShrink: 1 }} elevation={6}>
       <Toolbar>
-        <MenuIcon />
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          toDo
+        </Typography>
         <Box sx={{ flexGrow: 1 }} />
         <Button color="inherit">Login</Button>
       </Toolbar>
