82bb65395c9b83c67274ee479edd8d28147dd5af
MetaMatt <matt@metatech.dev>
2023-04-28 17:37:59 -0700

    fix(Header): use flexbox instead of margin auto, mui api instead of sx

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,4 +1,4 @@
-import { Toolbar } from "@mui/material";
+import { Toolbar, Box } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import MenuIcon from "@mui/icons-material/Menu";
 import Button from "@mui/material/Button";
@@ -8,7 +8,8 @@ const Header = () => {
     <AppBar position="static" sx={{ flexShrink: 1 }} elevation={6}>
       <Toolbar>
         <MenuIcon />
-        <Button sx={{ color: "white", marginLeft: "auto" }}>Login</Button>
+        <Box sx={{ flexGrow: 1 }} />
+        <Button color="inherit">Login</Button>
       </Toolbar>
     </AppBar>
   );
