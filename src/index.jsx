6358d9f7e2f21534f6899c0623f0ca557808f444
Zaq Reynolds <zaqreynolds@gmail.com>
2023-12-11 17:00:18 -0800

    feat: Add @auth0/auth0-react package
    This commit adds the "@auth0/auth0-react" package as a dependency in both "package-lock.json" and "package.json". The version added is "^2.2.4". Additionally, in "src/index.jsx", the Auth0Provider component from "@auth0/auth0-react" is imported and wrapped around the existing AppSettingsProvider component to enable authentication functionality.

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { AppSettingsProvider } from "./contexts/AppSettingsContext";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <AppSettingsProvider>
-    <App />
-  </AppSettingsProvider>
+  <Auth0Provider
+    domain="metatech-todo-dev.us.auth0.com"
+    clientId="mg6cH2lq8jIEpEzIqgdpP3G2pethhWJq"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <AppSettingsProvider>
+      <App />
+    </AppSettingsProvider>
+  </Auth0Provider>
 );
 
 reportWebVitals();
