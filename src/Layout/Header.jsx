265102377c1c8ca0610d4c45ed968bffcacc3d62
Zaq Reynolds <zaqreynolds@gmail.com>
2023-12-23 16:11:59 -0800

    feat(avatar): created logic to render profile pic or initials

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,16 +1,33 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Toolbar, Box, IconButton, Typography } from "@mui/material";
+import {
+  Toolbar,
+  Box,
+  IconButton,
+  Typography,
+  Avatar,
+  Tooltip,
+} from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import MenuIcon from "@mui/icons-material/Menu";
 import Button from "@mui/material/Button";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import DarkModeContext from "../contexts/AppContext";
 import BedtimeOutlinedIcon from "@mui/icons-material/BedtimeOutlined";
 import BedtimeOffOutlinedIcon from "@mui/icons-material/BedtimeOffOutlined";
 
 const Header = () => {
   const { isDarkMode, toggleDarkMode } = useContext(DarkModeContext);
-  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
+
+  const getInitials = () => {
+    return user?.name
+      .split(" ")
+      .map((n) => n[0])
+      .join("")
+      .toUpperCase();
+  };
+
+  const [anchorEl, setAnchorEl] = useState(null);
 
   return (
     <AppBar position="static" elevation={2}>
@@ -28,7 +45,15 @@ const Header = () => {
           ToDo
         </Typography>
         <Box sx={{ flexGrow: 1 }} />
-        {isAuthenticated ? (
+        <Tooltip title="Account settings" placement="left">
+          <IconButton
+            onClick={(e) => setAnchorEl(e.currentTarget)}
+            size="small"
+          >
+            <Avatar src={user?.picture || null}>{getInitials()}</Avatar>
+          </IconButton>
+        </Tooltip>
+        {/* {isAuthenticated ? (
           <Button
             color="inherit"
             onClick={() =>
@@ -48,7 +73,7 @@ const Header = () => {
           aria-label="Toggle Dark Mode"
         >
           {isDarkMode ? <BedtimeOffOutlinedIcon /> : <BedtimeOutlinedIcon />}
-        </IconButton>
+        </IconButton> */}
       </Toolbar>
     </AppBar>
   );
