08f3c39fb271084274c8436340c2a6caf47389bd
Zaq Reynolds <zaqreynolds@gmail.com>
2024-01-23 15:27:22 -0800

    feat(Header): Update margin in Typography component
    - Changed the `mr` (margin-right) value from 1 to 3 in the Typography component inside the Header component.
    - Improves spacing and alignment of the text element.

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -3,7 +3,6 @@ import AppBar from "@mui/material/AppBar";
 import { useMemo } from "react";
 import { Link as RouterLink, useLocation } from "react-router-dom";
 import { Typography } from "@mui/material";
-
 import AccountMenu from "./AccountMenu";
 import NavMenu from "./NavMenu";
 import { OrganizationSwitcher } from "@clerk/clerk-react";
@@ -18,7 +17,7 @@ const Header = () => {
       <Toolbar>
         <NavMenu />
         {isHome ? (
-          <Typography variant="h6" sx={{ mr: 1 }}>
+          <Typography variant="h6" sx={{ mr: 3 }}>
             ToDo
           </Typography>
         ) : (
@@ -28,7 +27,7 @@ const Header = () => {
             sx={{
               textDecoration: "none",
               color: "inherit",
-              mr: 1,
+              mr: 3,
             }}
           >
             <Typography variant="h6">ToDo</Typography>
